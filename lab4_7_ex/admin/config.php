ed6650816886e66a5a52376f4038be5a19076de1
Kristinajfnb <159126968+Kristinajfnb@users.noreply.github.com>
2024-03-28 19:06:43 +0300

    first

---
+++ lab4_7_ex/admin/config.php
@@ -0,0 +1,5 @@
+
+<?php
+    $path = "C:/OSPanel/domains/semi/lab4_7_ex/admin"; // Это путь к вашему скрипту
+    $fileName = "C:/OSPanel/domains/semi/lab4_7_ex/admin/data/accounts.txt"; // Это путь к файлу accounts.txt на сервере
+?>
